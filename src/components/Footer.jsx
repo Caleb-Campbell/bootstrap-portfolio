edd2b4b1da4f4ac1ba30bacffa2e6005d281bfe7
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-01-31 09:56:03 -0700

    wip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Button, Container, Stack } from 'react-bootstrap'
+import { Button, Container, Nav, Stack } from 'react-bootstrap'
 
 export default function Footer({darkMode}) {
   return (
+    <>
     <Button style={{position: 'fixed', bottom: '1em', right: '1em', scale: '.7'}} className={darkMode ? 'fs-3 bg-light text-dark' : 'fs-3 bg-dark text-light'} href='#nav'>Top</Button>
+    <Nav>
+      
+    </Nav>
+    </>
+
   )
 }
